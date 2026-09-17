8199753b7e504181974e5e7a5539c2f3c4be8697
Ahmad Kiswani <ahmadbkiswani444@gmail.com>
2026-09-17 19:54:01 +0300

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,11 +1,20 @@
 /*
 ===========================================================================
-DDL Script: Create Silver Tables
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===========================================================================
 Script Purpose:
-This script creates tables in the 'silver' schema, dropping existing tables
-if they already exist.
-Run this script to re-define the DDL structure of 'bronze' Tables
+	This stored procedure performs the ETL (Extract, Transform, Load) process to
+	populate the 'silver' schema tables from the 'bronze' schema.
+Actions Performed:
+	- Truncates Silver tables.
+	- Inserts transformed and cleansed data from Bronze into Silver tables.
+
+Parameters:
+	None.
+	This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+	EXEC Silver.load_silver;
 ===========================================================================
 */
 
